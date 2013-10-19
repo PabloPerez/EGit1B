1eec4f8810216c595c1f8877e0e719de362f0226
Pablo Pérez <p.perezcarrascal@gmail.com>
2013-10-19 12:37:08 +0200

    CD4

--- src/ClaseDevelop1.java
+++ src/ClaseDevelop1.java
@@ -3,6 +3,20 @@ public class ClaseDevelop1 {
 
 	private int atributocambio2;
 	private int atributocambio3;
+	
+	//cambio4
+	public int getAtributocambio2() {
+		return atributocambio2;
+	}
+	public void setAtributocambio2(int atributocambio2) {
+		this.atributocambio2 = atributocambio2;
+	}
+	public int getAtributocambio3() {
+		return atributocambio3;
+	}
+	public void setAtributocambio3(int atributocambio3) {
+		this.atributocambio3 = atributocambio3;
+	}
 
 	
 }
