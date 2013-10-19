e2f0dd9c8c67b9dc83522227c01a8b88e0c342ca
Pablo Pérez <p.perezcarrascal@gmail.com>
2013-10-19 12:39:26 +0200

    CR1.03

--- src/ClaseDevelop1.java
+++ src/ClaseDevelop1.java
@@ -3,6 +3,7 @@ public class ClaseDevelop1 {
 
 	private int atributocambio2;
 	private int atributocambio3;
+	private String atributocambio103;
 	
 	//CAMBIO RELEASE102
 	public int getAtributocambio3() {
