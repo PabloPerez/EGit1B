41fd59fe2a6d4910ac1f257732173a5c748deaef
Pablo Pérez <p.perezcarrascal@gmail.com>
2013-10-19 13:11:34 +0200

    CR1.04

--- src/ClaseDevelop1.java
+++ src/ClaseDevelop1.java
@@ -4,6 +4,8 @@ public class ClaseDevelop1 {
 	private int atributocambio2;
 	private int atributocambio3;
 	private String atributocambio103;
+	//CAMBIORELEASE104
+	private int atributocambio104;
 	
 	//CAMBIO RELEASE102
 	public int getAtributocambio3() {
@@ -12,6 +14,8 @@ public class ClaseDevelop1 {
 	public void setAtributocambio3(int atributocambio3) {
 		this.atributocambio3 = atributocambio3;
 	}
+	
 
+	
 	
 }
